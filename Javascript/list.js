35d6fb47b8e398ad16b2e2bad16d6ac38bdb8898
shantanug-cuelogic <shantanu.gade@cuelogic.com>
2018-07-12 03:25:14 +0530

    Filter is done

--- Javascript/list.js
+++ Javascript/list.js
@@ -35,6 +35,10 @@ function createtodo(){
 }
 
 function showtable(){
+    // var new_tbody = document.createElement('tbody');
+    // populate_with_new_rows(new_tbody);
+    // old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
+    // alert("xyz");
 
 for(var append=0;append<data[index].Todo.length;append++){
 
@@ -137,5 +141,130 @@ function done(x){
  localStorage.setItem("data",JSON.stringify(data));
 }
 
+function displayoption(){
+    if(document.getElementById('filteroption').style.display=="none")
+    {  
+          document.getElementById('filteroption').style.display="inline";
+    }
+    else
+    {
+        document.getElementById('filteroption').style.display="none";
+    }     
+}
+
+function showfiltertable (fi)
+{
+    
+
+        var x = document.createElement("TR");
+        x.setAttribute("id","listtr"+fi);
+        document.getElementById("listtable").appendChild(x);
+        
+        
+            var a =document.createElement("TD");
+            var input = document.createElement("input");
+            input.setAttribute("type","checkbox");
+            input.setAttribute("id","selectdata"+fi);
+            //input.setAttribute("onclick","c");
+            //var b =document.createTextNode(input);
+            a.appendChild(input);
+            document.getElementById("listtr"+fi).appendChild(a);
+        
+            var s =document.createElement("TD");
+            var t =document.createTextNode(fi);
+            s.appendChild(t);   
+            document.getElementById("listtr"+fi).appendChild(s);
+        
+        
+        
+            var c =document.createElement("TD");
+            var d =document.createTextNode(data[index].Todo[fi].Name);
+            c.appendChild(d);   
+            document.getElementById("listtr"+fi).appendChild(c);
+        
+            var e =document.createElement("TD");
+            var f =document.createTextNode(data[index].Todo[fi].Category);
+            e.appendChild(f);
+            document.getElementById("listtr"+fi).appendChild(e);
+        
+            var g =document.createElement("TD");
+            var h =document.createTextNode(data[index].Todo[fi].Duedate);
+            g.appendChild(h);
+            document.getElementById("listtr"+fi).appendChild(g);
+        
+            var i =document.createElement("TD");
+            var j =document.createTextNode(data[index].Todo[fi].Reminder);
+            i.appendChild(j);
+            document.getElementById("listtr"+fi).appendChild(i);
+        
+            var k =document.createElement("TD");
+            var l =document.createTextNode(data[index].Todo[fi].Public);
+            k.appendChild(l);
+            document.getElementById("listtr"+fi).appendChild(k);
+        
+            var m =document.createElement("TD");
+            var n =document.createTextNode(data[index].Todo[fi].Todonote);
+            m.appendChild(n);
+            document.getElementById("listtr"+fi).appendChild(m);
+        
+            var o =document.createElement("TD");
+            var p = document.createElement("P");
+            p.setAttribute("id","status"+fi);
+            var r = document.createTextNode(data[index].Todo[fi].Status);
+            p.appendChild(r);
+            o.appendChild(p);
+            document.getElementById("listtr"+fi).appendChild(o);
+        
+            var q =document.createElement("TD");
+            var button = document.createElement("Input");
+            button.setAttribute("type","Button");
+            button.setAttribute("id",+fi+".button");
+            button.setAttribute("value","Done");
+            button.setAttribute("onclick","done(this.id)")
+            
+            //var r =document.createTextNode("data");
+            q.appendChild(button);
+            document.getElementById("listtr"+fi).appendChild(q);
+     alert("stop");
+        }
+        
+        
 
 
+
+
+function filterresult(){
+
+    for(var k=0;k<data[index].Todo.length;k++){
+        var Table = document.getElementById("listtr"+k);
+      Table.innerHTML = "";
+      }
+
+      var method = document.getElementById("filteroption").value;
+      if(method == "pending")
+      {
+        for(var i=0;i<data[index].Todo.length;i++)
+          {
+       
+            if(data[index].Todo[i].Status=="Pending")
+                {
+                console.log(i);
+                showfiltertable(i);
+                 }
+          }
+      }
+
+      if(method == "complete")
+      {
+        for(var i=0;i<data[index].Todo.length;i++)
+          {
+       
+            if(data[index].Todo[i].Status=="Completed")
+                {
+                console.log(i);
+                showfiltertable(i);
+                 }
+          }
+      }
+
+}
