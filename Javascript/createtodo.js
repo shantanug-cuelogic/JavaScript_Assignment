62b1b261c0f4ffe52dccc3197451ef759dcc7407
shantanu <shantanu.gade@cuelogic>
2018-07-11 19:54:18 +0530

    create todo is done

--- Javascript/createtodo.js
+++ Javascript/createtodo.js
@@ -1,5 +1,14 @@
 var user = localStorage.getItem("username");
 var data = JSON.parse(localStorage.getItem("data"));
+var index;
+for(var i=0; i<data.length;i++ )
+{
+    if(user == data[i].Email)
+    {
+        index=i;
+        break;
+    }
+}
 
 function tolist(){
     window.location.href="list.html";
@@ -32,6 +41,48 @@ else
 
 }
 
+
+
 function addtodo(){
-    alert("in function");
+var Name= document.getElementById("tname").value;
+var Category= document.getElementById("category").value;
+var Duedate= document.getElementById("duedate").value;
+
+var rcheck = document.getElementById("remindercheck");
+
+if(rcheck.checked==true)
+    {
+    var Reminder=document.getElementById("remindedate").value;
+    }
+else
+    {
+    var Reminder = "No Reminder Set";
+    }
+
+var pcheck= document.getElementById("publiccheck");
+
+if(pcheck.checked == true)
+    {
+        
+    var Public = "Todo is Public";
+    }
+else
+    {
+    var Public = "Todo is Not Public";
+    }
+
+var Todonote= document.getElementById("todonote").value;
+
+var todoobj = {
+    Name:Name,
+    Category:Category,
+    Duedate:Duedate,
+    Reminder:Reminder,
+    Public:Public,
+    Todonote:Todonote
 }
+
+data[index].Todo.push(todoobj);
+localStorage.setItem("data",JSON.stringify(data));
+
+}// addtodo()
